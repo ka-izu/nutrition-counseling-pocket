66e63778403c4f374c848047f69267f1aa26bdfb
USER <user@USERnoMacBook-Pro.local>
2026-01-09 11:19:44 +0900

    style: RuboCop offensesの解決

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   # Devise の Strong Parameters を拡張し、
   # サインアップ時に name パラメータを許可
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
   end
 
   # ユーザー登録（サインアップ）完了後のリダイレクト先を指定
