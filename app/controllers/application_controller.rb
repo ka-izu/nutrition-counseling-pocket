4efa07bab0f7ed44665b408a99c0b0abc9adc75c
USER <user@USERnoMacBook-Pro.local>
2026-01-09 11:03:33 +0900

    feat: Strong Parameterにnameを追加、サインアップ成功時の遷移先設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,26 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # Devise の Strong Parameters を拡張し、
+  # サインアップ時に name パラメータを許可
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  # ユーザー登録（サインアップ）完了後のリダイレクト先を指定
+  # デフォルトでは自動ログインされるが、
+  # 本アプリではログイン画面へ遷移
+  def after_sign_up_path_for(resource)
+    new_user_session_path
+  end
+
+  # サインアウト後のリダイレクト先を指定
+  # 以下を記述しなければ、ルートパスに遷移
+  # def after_sign_out_path_for(resource_or_scope)
+  # end
 end
