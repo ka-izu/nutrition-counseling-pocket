b21ef29e0d20af18cf0b102163293218de49a1d1
USER <user@USERnoMacBook-Pro.local>
2026-01-11 11:35:40 +0900

    chore: rememberableモジュール、カラムをUserモデルから削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_01_08_121233) do
+ActiveRecord::Schema[7.2].define(version: 2026_01_11_022032) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,7 +20,6 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_08_121233) do
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
