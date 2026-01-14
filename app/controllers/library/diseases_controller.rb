f98447d58e931b9bb34b8fd3a1f73fe8caecd300
USER <user@USERnoMacBook-Pro.local>
2026-01-14 21:15:05 +0900

    chore: RuboCop offensesの解決

--- app/controllers/library/diseases_controller.rb
+++ app/controllers/library/diseases_controller.rb
@@ -1,5 +1,5 @@
 class Library::DiseasesController < Library::BaseLibraryController
   def index
-    @diseases = Disease.where(user_id: [nil, current_user.id])
+    @diseases = Disease.where(user_id: [ nil, current_user.id ])
   end
 end
