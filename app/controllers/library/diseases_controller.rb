81e0fe7310b0c168d1833499733f8c1700ee0da7
ka-izu <i.kanoads@gmail.com>
2026-02-26 14:34:11 +0900

    feat: 疾患名作成機能を追加

--- app/controllers/library/diseases_controller.rb
+++ app/controllers/library/diseases_controller.rb
@@ -2,4 +2,23 @@ class Library::DiseasesController < Library::BaseLibraryController
   def index
     @diseases = Disease.where(user_id: [ nil, current_user.id ])
   end
+
+  def new
+    @disease = Disease.new
+  end
+
+  def create
+    @disease = current_user.diseases.build(disease_params)
+
+    if @disease.save
+      redirect_to library_diseases_path,
+      notice: t("defaults.flash_message.created", item: Disease.model_name.human)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def disease_params
+    params.require(:disease).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "pages#top"
   namespace :library do
-    resources :diseases, only: %i[index] do
+    resources :diseases, only: %i[index new create] do
       resources :teaching_materials, except: %i[show]
     end
   end
