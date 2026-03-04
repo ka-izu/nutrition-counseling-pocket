5b4d0108d81dab70e9a9c65f1e8c2f9faf5a0121
ka-izu <i.kanoads@gmail.com>
2026-03-04 13:38:47 +0900

    feat: usersテーブルに provider と uid を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_02_24_125113) do
+ActiveRecord::Schema[7.2].define(version: 2026_03_04_020028) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -97,7 +97,10 @@ ActiveRecord::Schema[7.2].define(version: 2026_02_24_125113) do
     t.datetime "reset_password_sent_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
