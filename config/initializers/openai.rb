cf7bfe8d9c8c3d23a196d5a8be0ed2fb8fad66ab
ka-izu <i.kanoads@gmail.com>
2026-03-15 15:17:14 +0900

    fix: OPENAI_API_KEY未設定時のエラー回避とOpenAIログ設定を追加

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,6 +1,6 @@
 require "openai"
 
 OpenAI.configure do |config|
-  config.access_token = ENV.fetch("OPENAI_API_KEY")
-  config.log_errors = true # 開発時にエラーを確認できるようにする。TODO: 本番環境ではfalseにする
+  config.access_token =  ENV["OPENAI_API_KEY"]
+  config.log_errors = Rails.env.development?
 end
