46b5a62eb72f7131494d98951fe341faf346b943
ka-izu <i.kanoads@gmail.com>
2026-03-23 22:12:13 +0900

    test: ナレッジメモ新規作成のリクエストスペックを追加

--- spec/requests/library/knowledge_memos_spec.rb
+++ spec/requests/library/knowledge_memos_spec.rb
@@ -46,4 +46,66 @@ RSpec.describe "Library::KnowledgeMemos", type: :request do
       end
     end
   end
+
+  describe "POST /library/diseases/:disease_id/knowledge_memos" do
+    before do
+      sign_in user
+    end
+
+    context "正常系" do
+      let(:valid_params) do
+        {
+          knowledge_memo: {
+            title: "テストメモ",
+            content: "内容"
+          }
+        }
+      end
+
+      it "メモが作成されること" do
+        expect {
+          post library_disease_knowledge_memos_path(disease), params: valid_params
+        }.to change(KnowledgeMemo, :count).by(1)
+      end
+
+      it "userとdiseaseが紐づくこと" do
+        post library_disease_knowledge_memos_path(disease), params: valid_params
+        memo = KnowledgeMemo.last
+
+        expect(memo.user).to eq(user)
+        expect(memo.disease).to eq(disease)
+      end
+
+      it "一覧画面へリダイレクトされること" do
+        post library_disease_knowledge_memos_path(disease), params: valid_params
+
+        expect(response).to redirect_to(
+          library_disease_knowledge_memos_path(disease)
+        )
+      end
+    end
+
+    context "異常系" do
+      let(:invalid_params) do
+        {
+          knowledge_memo: {
+            title: "",
+            content: "内容"
+          }
+        }
+      end
+
+      it "メモが作成されないこと" do
+        expect {
+          post library_disease_knowledge_memos_path(disease), params: invalid_params
+        }.not_to change(KnowledgeMemo, :count)
+      end
+
+      it "newページが再表示されること" do
+        post library_disease_knowledge_memos_path(disease), params: invalid_params
+
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
 end
