9643b202e4c056944e2f57c85671a1c019c52931
ka-izu <i.kanoads@gmail.com>
2026-04-09 10:38:05 +0900

    feat: アカウント更新機能の追加

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,4 +1,27 @@
 class AccountsController < ApplicationController
+  before_action :authenticate_user!
+
   def show
+    @account = current_user
+  end
+
+  def edit
+    @account = current_user
+  end
+
+  def update
+    @account = current_user
+    if @account.update(account_params)
+      redirect_to account_path,
+      notice: t("defaults.flash_message.updated", item: "アカウント情報")
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def account_params
+    params.require(:user).permit(:name, :email)
   end
 end
